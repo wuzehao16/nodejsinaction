6e2b02a2ab49c058edf14897755a939432751dc6
wuzehao16 <wuzehao16@qq.com>
2018-05-16 22:01:40 +0800

    add hello world http server

---
+++ 4/hello.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+var server = http.createServer((req, res) => {
+  var url = "http://www.baidu.com"
+  var body = `redirect to <a href=${url}>baidu</a>`
+  res.setHeader('Location', url)
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Content-Length', body.length)
+  res.end(body)
+})
+server.listen(3000)
