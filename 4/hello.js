0ef647c581c6cff8f4b5d48327697c3052c72983
wuzehao16 <wuzehao16@qq.com>
2018-05-16 22:25:40 +0800

    change length to byteLength

--- 4/hello.js
+++ 4/hello.js
@@ -1,11 +1,10 @@
 const http = require('http');
-
 var server = http.createServer((req, res) => {
   var url = "http://www.baidu.com"
   var body = `redirect to <a href=${url}>baidu</a>`
   res.setHeader('Location', url)
   res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Content-Length', body.length)
+  res.setHeader('Content-Length', Buffer.byteLength(body))
   res.end(body)
 })
 server.listen(3000)
